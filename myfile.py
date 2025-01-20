b9b22d58a4fac75c8bd0b973f1786b2cf4df3113
Md Jakaria <jakaria.dcc11@gmail.com>
2025-01-20 14:18:44 -0500

    task 5-8 completed

---
+++ myfile.py
@@ -0,0 +1,12 @@
+def calculate_average(numbers):
+    total = sum(numbers)
+    count = len(numbers)
+    average = total / count
+    return average
+
+# Intentionally passing a string instead of a list of numbers
+result = calculate_average([1,2,3,4,5,6,7,8,9,10])
+print("The average is:", result)
+
+if result > 5
+    print("The average is greater than 5")
